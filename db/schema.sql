1af422b5575416b1f77efbee9b8a9eaaba6ed937
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-09-15 14:51:29 -0500

    Added starter code

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS project2_db;
+
+CREATE DATABASE project2_db;
---
+++ seeds/seed.js
@@ -0,0 +1,18 @@
+const { Sequelize } = require("sequelize");
+const sequelize = require("../config/connection");
+const { User } = require("../models");
+
+const userData = require("./userData.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
