12ae4fa6810dadb04cd406de1af5612c55fdeb54
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-18 19:51:09 -0500

    added home routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoute.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+
 
 module.exports = router;
