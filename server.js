9ae689f20426a1c8ae8641b8fb4c4ebf28748e37
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-09-18 20:40:45 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const session = require("express-session");
 const routes = require("./controllers");
+const path = require("path");
+const exphbs = require("express-handlebars");
 
 const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session - Store);
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: "Super secret secret",
-  cookie: [],
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: true,
+    sameSite: strict,
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -20,8 +27,12 @@ const sess = {
 
 app.use(session(sess));
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, public)));
 
 app.use(routes);
 
