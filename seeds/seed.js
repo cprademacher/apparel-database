767e587839c9b9f3688d8b3a5451c61a7d8d0f38
Cody Rademacher <66492356+cprademacher@users.noreply.github.com>
2023-09-20 20:10:37 -0500

    Added product and category seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,25 @@
 const { Sequelize } = require("sequelize");
 const sequelize = require("../config/connection");
-const { User } = require("../models");
+const { User, Product, Category } = require("../models");
 
 const userData = require("./userData.json");
+const productData = require("./productData.json");
+const categoryData = require("./categoryData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
+  const categories = await Category.bulkCreate(categoryData, {
+    individualHooks: true,
+    returning: true,
+  });
+
   const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  const products = await Product.bulkCreate(productData, {
     individualHooks: true,
     returning: true,
   });
