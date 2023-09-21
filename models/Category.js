4e1f3807f318c2338ede56039c1b3cd91c7c7c90
Brandon Williams <brandonwilliams.88@yahoo.com>
2023-09-20 20:31:31 -0500

    commit for changes made to fix node seeds/seed.js errors

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,25 @@ const sequelize = require("../config/connection");
 
 class Category extends Model {}
 
-Category.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  category_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "category",
+  }
+);
 
 module.exports = Category;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const routes = require("./controllers");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
@@ -10,13 +11,15 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
   secret: "Super secret secret",
   cookie: {
     maxAge: 24 * 60 * 60 * 1000,
     httpOnly: true,
     secure: false,
-    sameSite: strict,
+    sameSite: "strict",
   },
   resave: false,
   saveUninitialized: true,
@@ -32,7 +35,7 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, public)));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
