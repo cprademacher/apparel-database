208e5fcfd30816ea6f7cbaa4de537439f8b05e93
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-25 14:59:53 -0500

    added a route for collections

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -10,8 +10,28 @@ router.get("/", withAuth, async (req, res) => {
     const categories = categoryData.map((category) => category.get({ plain: true }));
     console.log(categories, isProduct);
     res.render("homepage", {
-      posts,
+      categories,
       loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/collections", withAuth, async (req, res) => {
+  const isCollections = true;
+  try {
+    const collectionsData = await Tag.findAll({
+      include: [{ model: Product }
+      ],
+    });
+    const collections = collectionsData.map((collections) => collections.get({ plain: true }));
+    console.log(collectionsData, collections);
+    res.render("collections", {
+      collections,
+      loggedIn: req.session.loggedIn,
+      isCollections
     });
   } catch (err) {
     console.log(err);
