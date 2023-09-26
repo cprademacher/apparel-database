d659ab87a9aff93b84d09a8d2313aa455fc7d37a
JakeHowdeshell <135374279+JakeHowdeshell@users.noreply.github.com>
2023-09-26 18:41:00 -0500

    updated to accept url

--- public/js/update.js
+++ public/js/update.js
@@ -11,62 +11,82 @@ function showForm(categoryId, productId) {
   const productForm = document.querySelector(".update-product-form");
 
   productForm.style.display = "block";
-
+  const fileInput = document.getElementById("update-product-url");
   const productSubmitButton = document.querySelector(".update-product-button");
   productSubmitButton.addEventListener("click", () => {
-    const updatedProductName = document.querySelector(".update-product-name").value;
-    const updatedProductStock = document.querySelector(".update-product-stock").value;
-    const updatedProductPrice = document.querySelector(".update-product-price").value;
-    // const newProductUrl = document.querySelector(".new-product-url").value;
-    updateProduct(
-      productId,
-      categoryId,
-      updatedProductName,
-      updatedProductStock,
-      updatedProductPrice,
-      // newProductUrl
-    );
+    const updatedProductName = document.querySelector(
+      ".update-product-name"
+    ).value;
+    const updatedProductStock = document.querySelector(
+      ".update-product-stock"
+    ).value;
+    const updatedProductPrice = document.querySelector(
+      ".update-product-price"
+    ).value;
+    const updatedProductUrl = fileInput.files[0];
+    if (updatedProductUrl) {
+      console.log("Selected file:", updatedProductUrl);
+      updateProduct(
+        productId,
+        categoryId,
+        updatedProductName,
+        updatedProductStock,
+        updatedProductPrice,
+        updatedProductUrl
+      );
+    } else {
+      alert("Please select a file before uploading.");
+    }
   });
 }
 
-function updateProduct(productId, categoryId,
+function updateProduct(
+  productId,
+  categoryId,
   updatedProductName,
   updatedProductStock,
-  updatedProductPrice,) {
-  if (productId && categoryId &&
+  updatedProductPrice,
+  updatedProductUrl
+) {
+  if (
+    productId &&
+    categoryId &&
     updatedProductName &&
     updatedProductStock &&
-    updatedProductPrice) {
-  fetch(`/api/products/${productId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      product_name: updatedProductName,
-      stock: updatedProductStock,
-      price: updatedProductPrice,
-    }),
-  })
-    .then((response) => {
-      console.log(response);
-      if (response.ok) {
-        if (response.redirected) {
-          window.location.assign(response.url);
-          return;
+    updatedProductPrice &&
+    updatedProductUrl
+  ) {
+    fetch(`/api/products/${productId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        product_name: updatedProductName,
+        stock: updatedProductStock,
+        price: updatedProductPrice,
+        url: "/assets/" + updatedProductUrl.name,
+      }),
+    })
+      .then((response) => {
+        console.log("updateProduct", response);
+        if (response.ok) {
+          if (response.redirected) {
+            window.location.assign(response.url);
+            return;
+          }
+          return response.json();
+        } else {
+          throw new Error("Failed to update post");
         }
-        return response.json();
-      } else {
-        throw new Error("Failed to update post");
-      }
-    })
-    .then((updatedProduct) => {
-      console.log("New post:", updatedProduct);
-      window.location.assign(`/api/category/${categoryId}`);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+      })
+      .then((updatedProduct) => {
+        console.log("New post:", updatedProduct);
+        window.location.assign(`/api/category/${categoryId}`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
   // else {
   //   const updateMessage = document.querySelector("#message");
